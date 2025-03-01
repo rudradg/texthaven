d15b2551ef55aa107511bc2263e603e9bb51abff
rudradg <rudradg@tutamail.com>
2025-02-28 20:04:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,42 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",  
+  "/script.js",  
+  "/icon.png"     
+];
+
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker Installed");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    console.log("Fetching:", event.request.url);
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cache) => cache !== CACHE_NAME)
+          .map((cache) => caches.delete(cache))
+      );
+    })
+  );
+});
+
+
