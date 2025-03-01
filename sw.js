9823aa0dbae864d2a1ab95791261c46f6270fbdf
rudradg <rudradg@tutamail.com>
2025-02-28 20:05:28 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,9 @@ const urlsToCache = [
   "/index.html",
   "/styles.css",  
   "/script.js",  
-  "/icon.png"     
+  "/icon.png", 
+  "@import url('https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+"
 ];
 
 
