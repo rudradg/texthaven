8d725c160c49100a25895167f9afc91ec4b5985d
rudradg <rudradg@tutamail.com>
2025-02-28 22:11:20 -0500

    fix

--- typehaven.js
+++ typehaven.js
@@ -55,7 +55,7 @@ function openText() {
         alert("Please select a file!");
     }
 }
-navigator.serviceWorker.register('/service-worker/sw.js').then((registration) => {
+navigator.serviceWorker.register('/sw.js').then((registration) => {
   console.log('Service Worker registered with scope:', registration.scope);
 }).catch((error) => {
   console.log('Service Worker registration failed:', error);
