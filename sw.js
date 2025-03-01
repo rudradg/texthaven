b2f6038ce58c0ae71e6c354a26c53348c2f1a38d
rudradg <rudradg@tutamail.com>
2025-02-28 21:22:21 -0500

    fixed sw again

--- sw.js
+++ sw.js
@@ -1,44 +1,7 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",  
-  "/script.js",  
-  "/icon.png", 
-  "@import url('https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-"
-];
-
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log("Service Worker Installed");
 });
-
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  console.log("Fetching:", event.request.url);
 });
-
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cache) => cache !== CACHE_NAME)
-          .map((cache) => caches.delete(cache))
-      );
-    })
-  );
-});
-
-
